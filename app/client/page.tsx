b1626a1b93117b1bdc656ce9a56077b43086e6a9
ZhubanyshZh <zubanyszarylkasyn@gmail.com>
2025-06-29 23:17:08 +0500

    fix: executor name

--- app/client/page.tsx
+++ app/client/page.tsx
@@ -758,10 +758,10 @@ export default function ClientDashboard() {
                                 <span className="truncate font-medium">{formatDate(request.created_date)}</span>
                               </div>
 
-                              {request.executor.user.full_name ? (
+                              {request.executor_id ? (
                                   <div className="flex items-center gap-2 text-gray-600 bg-gray-50 p-2 rounded-lg">
                                     <User className="w-4 h-4 flex-shrink-0 text-purple-500" />
-                                    <span className="truncate font-medium">{request.executor.user.full_name}</span>
+                                    <span className="truncate font-medium">{request.executor_id}</span>
                                   </div>
                               ) : (
                                   <div className="flex items-center gap-2 text-gray-400 bg-gray-50 p-2 rounded-lg">
